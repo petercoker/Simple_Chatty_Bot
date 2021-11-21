f7ec49f7ebb63d385f67dca77f9762ea5d1fe2f3
peter <peter.coker@protonmail.com>
2021-11-21 09:13:46 +0000

    completed stage 2

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,11 +1,18 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
-        String botName = "Peter";
-        int birthYear = 2021;
+        Scanner scanner = new Scanner(System.in);
+        String yourName;
 
-        System.out.printf("Hello! My name is %s.\n", botName);
-        System.out.printf("I was created in %s.", birthYear);
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        // reading a name
+        yourName = scanner.nextLine();
+        System.out.printf("What a great name you have, %s!", yourName);
     }
 }
