713e4a36d972fcce32c8f249052e5b3ef276a2f7
peter <peter.coker@protonmail.com>
2021-11-21 09:00:45 +0000

    Initial commit

---
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -0,0 +1,11 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        String botName = "Peter";
+        int birthYear = 2021;
+
+        System.out.printf("Hello! My name is %s.\n", botName);
+        System.out.printf("I was created in %s.", birthYear);
+    }
+}
